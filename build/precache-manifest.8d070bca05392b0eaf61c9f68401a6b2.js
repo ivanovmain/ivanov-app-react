e8ce54f9c386f2bc46b956020957d6e14e81b695
Vladislav <ivanovvvmain@yandex.ru>
2020-02-02 12:07:40 +0700

    new token fix

---
+++ build/precache-manifest.8d070bca05392b0eaf61c9f68401a6b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac87a45192cca2fa0b6018357eb37ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f3a73a4558e31edaaa",
+    "url": "/static/css/main.60530cb2.chunk.css"
+  },
+  {
+    "revision": "13bc1c268f9e10b02edf",
+    "url": "/static/js/2.b5220e4a.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.b5220e4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15f3a73a4558e31edaaa",
+    "url": "/static/js/main.42292415.chunk.js"
+  },
+  {
+    "revision": "070db8a3c8ae3e511328",
+    "url": "/static/js/runtime-main.2546e0bf.js"
+  }
+]);
